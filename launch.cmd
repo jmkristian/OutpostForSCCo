106b67dd0e23b2bf92a4d845b26fa678d67ee47c
John Kristian <km6lqz@arrl.net>
2018-10-01 15:45:02 -0700

    Make lauch.cmd less obnoxious.

--- launch.cmd
+++ launch.cmd
@@ -1,7 +1,7 @@
-@echo Sorry, this is ugly on Windows XP. Newer versions of Windows work better.
+@echo Starting your browser. Please stand by.
 @rem This ugly substitute for launch.vbs is used only on Windows XP.
 @rem Why? Firefox on XP crashes when "lauch.vbs open" runs "start http://etc."
 @rem If you know how to prevent this, please contribute to
 @rem https://github.com/jmkristian/OutpostForLAARES/issues/1
 @cd "%~dp0"
-@start /B bin\Outpost_for_LAARES.exe %*
+@start /MIN bin\Outpost_for_LAARES.exe %*
