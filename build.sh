f27ada1da520fcc7c9180f18e601583e2e58da03
John Kristian <jmkristian@gmail.com>
2019-02-02 15:59:35 -0800

    Resume building OutpostForLAARES.

--- build.sh
+++ build.sh
@@ -18,8 +18,13 @@ if [ ! -e pack-it-forms ]; then
 fi
 
 rm -rf built; mkdir -p built/addons
+node bin/Outpost_Forms.js build Los_Altos "bin\\LAARES_Forms.exe"
+"C:/Program Files (x86)/NSIS/makensis.exe" setup-LAARES.nsi || exit $?
+
+rm -rf built; mkdir -p built/addons
 node bin/Outpost_Forms.js build HTTP "bin\\HTTP_server.exe"
 "C:/Program Files (x86)/NSIS/makensis.exe" setup-HTTP.nsi || exit $?
+
 rm -rf built; mkdir -p built/addons
 node bin/Outpost_Forms.js build Enhanced "bin\\SCCoPIFO.exe"
 "C:/Program Files (x86)/NSIS/makensis.exe" setup-SCCo.nsi || exit $?
