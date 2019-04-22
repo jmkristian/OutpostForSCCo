d9a3d771f4974160a0bd43c89d332dd49e76619e
John Kristian <jmkristian@gmail.com>
2019-04-22 16:21:02 -0700

    Start version 2.7.

--- build.cmd
+++ build.cmd
@@ -1,7 +1,7 @@
 @REM This can't be a Bash script, because quoting /DDisplayName doesn't work right.
 
 @set VersionMajor=2
-@set VersionMinor=6
+@set VersionMinor=7
 
 @rem rmdir /Q /S built
 @rem mkdir built
