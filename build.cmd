ebd9816fffcbd13b089c6bab08df27adb4578997
John Kristian <jmkristian@gmail.com>
2019-07-30 10:52:07 -0700

    Start version 2.18. Upgrade pack-it-forms to vSCCo.27.

--- build.cmd
+++ build.cmd
@@ -1,7 +1,7 @@
 @REM This can't be a Bash script, because quoting /DDisplayName doesn't work right.
 
 @set VersionMajor=2
-@set VersionMinor=17
+@set VersionMinor=18
 
 @rem rmdir /Q /S built
 @rem mkdir built
