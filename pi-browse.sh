cd957bf4efcdd723029c22dc166c09cb6c0f0480
John Kristian <jmkristian@gmail.com>
2020-05-10 11:41:51 -0700

    On Raspberry Pi, suppress blather from chromium-browser.

--- pi-browse.sh
+++ pi-browse.sh
@@ -4,4 +4,6 @@ cd `dirname "$0"`
 {{PROGRAM_PATH}} dry-run {{PROGRAM_PATH}} || exit $?
 SERVER_PORT=`cat logs/server-port.txt`
 echo       chromium-browser http://127.0.0.1:$SERVER_PORT/manual
-DISPLAY=:0 chromium-browser http://127.0.0.1:$SERVER_PORT/manual || exit $?
+DISPLAY=:0 chromium-browser http://127.0.0.1:$SERVER_PORT/manual\
+    | fgrep -v "FontService unique font name matching request did not receive a response."\
+    || exit $?
