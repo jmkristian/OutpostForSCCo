d30ded090115c04a80ce9128fddbc19e03e71a08
John Kristian <jmkristian@gmail.com>
2020-05-10 12:46:03 -0700

    Build SF_ACS_forms for Raspberry Pi.

--- pi-build.sh
+++ pi-build.sh
@@ -3,49 +3,51 @@ VersionMajor=2
 VersionMinor=21b
 cd `dirname "$0"` || exit $?
 if [ ! -e node_modules ]; then
-    # https://docs.npmjs.com/cli/install
-    npm install                              || exit $?
+    npm install || exit $? # https://docs.npmjs.com/cli/install
 fi
 #if [ ! -e "pack-it-forms"/.git ]; then # Don't delete an experimental copy.
 #    rm -rf "pack-it-forms"
 #fi
 if [ ! -e "pack-it-forms" ]; then
     git clone https://github.com/jmkristian/pack-it-forms.git || exit $?
-    (cd "pack-it-forms" && git checkout vSCCo.30)
+    (cd "pack-it-forms" && git checkout SF-ACS)
     rm -rf "pack-it-forms"/.git*
 fi
-rm -rf built/SCCoPIFR/*
-mkdir -p built/SCCoPIFR/addons                  || exit $?
-mkdir    built/SCCoPIFR/bin                     || exit $?
-cp -p bin/*.ini bin/*.html built/SCCoPIFR/bin/  || exit $?
-node bin/Outpost_Forms.js build "$VersionMajor"."$VersionMinor"\
-   SCCoPIFO bin/SCCoPIFR.js "SCCo PackItForms for Raspberry Pi"\
-                                                || exit $?
-mv built/addons built/SCCoPIFR/                 || exit $?
-mv built/pi-browse.sh built/SCCoPIFR/browse.sh  || exit $?
-mv built/UserGuide.html built/SCCoPIFR/         || exit $?
-mv built/manual.html built/SCCoPIFR/bin         || exit $?
-cp -p bin/Outpost_Forms.js built/SCCoPIFR/bin/SCCoPIFR.js    || exit $?
-cp -pr package.json node_modules built/SCCoPIFR/bin/         || exit $?
-cp -pr pack-it-forms built/SCCoPIFR/         || exit $?
-cp -p *.png built/SCCoPIFR/pack-it-forms     || exit $?
+for ADDON in SCCoPIFO SF_ACS_forms "$@"; do
+    rm -rf   built/$ADDON/*
+    mkdir -p built/$ADDON/addons                 || exit $?
+    mkdir    built/$ADDON/bin                    || exit $?
+    cp -p bin/*.ini bin/*.html built/$ADDON/bin/ || exit $?
+    node bin/Outpost_Forms.js build "$VersionMajor"."$VersionMinor"\
+         "$ADDON" bin/$ADDON.js "PackItForms for Raspberry Pi"\
+        || exit $?
+    mv built/addons built/$ADDON/                || exit $?
+    mv built/pi-browse.sh built/$ADDON/browse.sh || exit $?
+    mv built/UserGuide.html built/$ADDON/        || exit $?
+    mv built/manual.html built/$ADDON/bin        || exit $?
+    cp -p bin/Outpost_Forms.js built/$ADDON/bin/$ADDON.js  || exit $?
+    cp -pr package.json node_modules built/$ADDON/bin/     || exit $?
+    cp -pr pack-it-forms built/$ADDON/           || exit $?
+    cp -p *.png built/$ADDON/pack-it-forms       || exit $?
 
-cd built/SCCoPIFR                            || exit $?
-find . -type f -name "*~" | xargs rm
-rm -rf .git
-mv pack-it-forms/pdf .                       || exit $?
-rm pdf/LOS-ALTOS-*.pdf
-rm pack-it-forms/form-los-altos-*.html
-mv pack-it-forms/resources/integration/scco/SCCoPIFO.launch addons/ || exit $?
-cd pack-it-forms/resources/integration       || exit $?
-mv scco/integration.js .                     || exit $?
-rm -r pacread scco                           || exit $?
-cd ../../..
-rm bin/cmd-convert.ini                       || exit $?
-node bin/SCCoPIFR.js install bin/SCCoPIFR.js || (cat logs/*-install.log; exit 2)
-rm -r addons/SCCoPIFO                        || exit $?
-rm bin/addon.ini bin/Aoclient.ini
-echo "$VersionMajor"."$VersionMinor" > version.txt
-chmod +x browse.sh bin/SCCoPIFR.js           || exit $?
-cd ..
-tar -czf SCCoPIFR.tar.gz SCCoPIFR            || exit $?
+    cd built/$ADDON                              || exit $?
+    find . -type f -name "*~" | xargs rm
+    rm -rf .git
+    mv pack-it-forms/pdf .                       || exit $?
+    rm pdf/LOS-ALTOS-*.pdf
+    rm pack-it-forms/form-los-altos-*.html
+    mv pack-it-forms/resources/integration/scco/$ADDON.launch addons/ || exit $?
+    cd pack-it-forms/resources/integration       || exit $?
+    mv scco/integration.js .                     || exit $?
+    rm -r pacread scco                           || exit $?
+    cd ../../..
+    rm bin/cmd-convert.ini                       || exit $?
+    node bin/$ADDON.js install bin/$ADDON.js || (cat logs/*-install.log; exit 1) || exit $?
+    rm -r addons/$ADDON                          || exit $?
+    rm bin/addon.ini bin/Aoclient.ini
+    echo "$VersionMajor"."$VersionMinor" > version.txt
+    chmod +x browse.sh bin/$ADDON.js             || exit $?
+    cd ..
+    tar -czf $ADDON.tar.gz $ADDON                || exit $?
+    cd ..
+done
