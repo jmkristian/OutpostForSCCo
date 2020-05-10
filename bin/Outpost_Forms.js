b428aa481bbabe93daa567d547729b05d02a4395
John Kristian <jmkristian@gmail.com>
2020-05-09 17:28:02 -0700

    Enable executing Outpost_Forms.js on Linux.

--- bin/Outpost_Forms.js
+++ bin/Outpost_Forms.js
@@ -1,4 +1,5 @@
-/* Copyright 2018, 2019 by John Kristian
+#!/usr/bin/node
+ /* Copyright 2018, 2019 by John Kristian
 
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
