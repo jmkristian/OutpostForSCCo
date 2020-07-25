91f41baeb6d613aed5d3b7fc74ea284589ce82bb
John Kristian <jmkristian@gmail.com>
2020-07-25 12:16:31 -0700

    Remove surplus lookup table.

--- bin/Outpost_Forms.js
+++ bin/Outpost_Forms.js
@@ -275,24 +275,6 @@ const NOT_FOUND = 404;
 const CHARSET = 'utf-8'; // for HTTP
 const ENCODING = CHARSET; // for files
 const EOL = '\r\n';
-const fromHex = {
-    '0': 0,
-    '1': 1,
-    '2': 2,
-    '3': 3,
-    '4': 4,
-    '5': 5,
-    '6': 6,
-    '7': 7,
-    '8': 8,
-    '9': 9,
-    'a': 10, 'A': 10,
-    'b': 11, 'B': 11,
-    'c': 12, 'C': 12,
-    'd': 13, 'D': 13,
-    'e': 14, 'E': 14,
-    'f': 15, 'F': 15
-};
 const htmlEntities = new AllHtmlEntities();
 const INI = { // patterns that match lines from a .ini file.
     comment: /^\s*;/,
