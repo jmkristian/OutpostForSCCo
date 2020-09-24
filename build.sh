c2b8bf3ba9e953b200c623ded392f50edb47bcd8
John Kristian <jmkristian@gmail.com>
2020-09-24 11:13:07 -0700

    Upgrade forms to vSCCo.35.

--- build.sh
+++ build.sh
@@ -22,7 +22,7 @@ for REPO in jmkristian/pack-it-forms "$@"; do
     fi
     if [ ! -e "$FORMS" ]; then
         git clone https://github.com/"$REPO".git || exit $?
-        (cd "$FORMS" && git checkout vSCCo.34)
+        (cd "$FORMS" && git checkout vSCCo.35)
         rm -rf "$FORMS"/.git*
     fi
     "$FORMS"/resources/integration/scco/build.cmd || exit
