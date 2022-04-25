60a761621798d0d852d92f8c5d2bac32e3934344
John Kristian <jmkristian@gmail.com>
2022-04-25 14:40:29 -0700

    Start version 3.9.

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 # This script can be executed by the bash that's packaged with git for Windows.
 cd `dirname "$0"` || exit $?
 export VersionMajor=3
-export VersionMinor=8
+export VersionMinor=9
 export VersionBeta=
 rm -rf built logs
 mkdir -p built/bin built/webToPDF
