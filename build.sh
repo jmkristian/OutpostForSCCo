6c5f036fdf771c2328a5bf201c712261932b4c17
John Kristian <jmkristian@gmail.com>
2024-02-29 02:42:07 -0800

    Upgrade pack-it-forms to vSCCo.48.

--- build.sh
+++ build.sh
@@ -23,7 +23,7 @@ for REPO in jmkristian/pack-it-forms "$@"; do
     fi
     if [ ! -e "$FORMS" ]; then
         git clone https://github.com/"$REPO".git || exit $?
-        (cd "$FORMS" && git checkout vSCCo.47)
+        (cd "$FORMS" && git checkout vSCCo.48)
         rm -rf "$FORMS"/.git*
     fi
     "$FORMS"/resources/integration/scco/build.sh ./buildInstaller.sh\
